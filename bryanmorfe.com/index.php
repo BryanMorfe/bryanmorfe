a4e8f65230b9b862d85dbeee1437e1c81d213577
Bryan Morfe <bryanmorfe@gmail.com>
2017-02-20 04:26:17 +0000

    Finished implementing footer

--- bryanmorfe.com/index.php
+++ bryanmorfe.com/index.php
@@ -1,77 +1,77 @@
 <!doctype html>
 <html>
- <head>
-  <title>Bryan Morfe - Computer Scientist</title>
-  <link href="css/structure.css" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Assistant:300|Catamaran:800|Noto+Sans|Palanquin:100|Raleway:200" rel="stylesheet">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="js/structure.js"></script>
-  <link rel="stylesheet" href="css/main.css">
- </head>
+	<head>
+  		<title>Bryan Morfe - Computer Scientist</title>
+  		<link href="css/structure.css" rel="stylesheet">
+  		<link href="https://fonts.googleapis.com/css?family=Assistant:300|Catamaran:800|Noto+Sans|Palanquin:100|Raleway:200" rel="stylesheet">
+  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  		<script src="js/structure.js"></script>
+  		<link rel="stylesheet" href="css/main.css">
+ 	</head>
 
- <body>
-  <header>
-   <div class="nav">
-    <div class="main-content">
-     <ul>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">Portfolio</a></li>
-      <li><a href="#">Projects</a></li>
-      <li><a href="#">About Me</a></li>
-      <li><a href="#">Contact</a></li>
-     </ul>
-     <button class="btn btn-md btn-action">Blog</button>
-     <button class="btn btn-md btn-option">Resume</button>
-    </div>
-   </div>
-  </header>
+ 	<body>
+  		<header>
+   			<div class="nav">
+    			<div class="main-content">
+     				<ul>
+      					<li><a href="#">Home</a></li>
+      					<li><a href="#">Portfolio</a></li>
+      					<li><a href="#">Projects</a></li>
+      					<li><a href="#">About Me</a></li>
+      					<li><a href="#">Contact</a></li>
+     				</ul>
+     				<button class="btn btn-md btn-action">Blog</button>
+     				<button class="btn btn-md btn-option">Resume</button>
+    			</div>
+   			</div>
+  		</header>
 
-  <main>
+  		<main>
 
-   <div id="welcome-section" class="clearfix">
-    <div class="main-content">
-     <h2 class="section-title">WELCOME</h2>
-     <div class="card card-flex-9 clickable-card">
-      <img src="imgs/mypic.png" class="profile-img-lg">
-      <h3 class="card-title">The Swift Programming Language</h3>
-      <p class="card-content">The Swift programming language has gained popularity very quickly during the last years. Since it was made open source in 2015. It became the #1 programming language in GitHub, where its source code is being currently held.</p>
-     </div>
-     <div class="card card-flex-3 clickable-card">
-      <img src="imgs/mypic.png" class="profile-img-md">
-      <h3 class="card-title">Bryan Morfe</h3>
-      <p class="card-content">I am...</p>
-     </div>
-    </div>
-   </div>
+   			<div id="welcome-section" class="clearfix">
+    			<div class="main-content">
+     				<h2 class="section-title">WELCOME</h2>
+     				<div class="card card-flex-9 clickable-card">
+      					<img src="imgs/mypic.png" class="profile-img-lg">
+      					<h3 class="card-title">The Swift Programming Language</h3>
+      					<p class="card-content">The Swift programming language has gained popularity very quickly during the last years. Since it was made open source in 2015. It became the #1 programming language in GitHub, where its source code is being currently held.</p>
+     				</div>
+     				<div class="card card-flex-3 clickable-card">
+      					<img src="imgs/mypic.png" class="profile-img-md">
+      					<h3 class="card-title">Bryan Morfe</h3>
+      					<p class="card-content">I am...</p>
+     				</div>
+    			</div>
+   			</div>
 
-   <div id="projects-section" class="clearfix">
-    <div class="main-content">
-     <h2 class="section-title">PROJECTS</h2>
-     <div class="card card-flex-6 clickable-card">
-      <figure class="card-header blue-tinted-header rounded-header">
-       <img src="imgs/ios-development.jpg">
-      </figure>
-      <h3 class="card-title">iOS Development</h3>
-      <p class="card-content">iOS Deveplopment has grown exponentially during the year of 2016. It appears more and more people are harnessing the power of mobile development, especially now that Apple's new programming language is gaining popularity.</p>
-     </div>
-     <div class="card card-flex-3 clickable-card">
-      <figure class="card-header green-tinted-header rounded-header">
-       <img src="imgs/ios-developer.jpg">
-      </figure>
-      <h3 class="card-title">iOS Development</h3>
-      <p class="card-content">iOS Deveplopment has grown exponentially during the year of 2016. It appears more and more people are harnessing the power of mobile development, especially now that Apple's new programming language is gaining popularity.</p>
-     </div>
-     <div class="card card-flex-3 clickable-card">
-      <figure class="card-header green-tinted-header rounded-header">
-       <img src="imgs/ios-developer.jpg">
-      </figure>
-      <h3 class="card-title">iOS Development</h3>
-      <p class="card-content">iOS Deveplopment has grown exponentially during the year of 2016. It$
-     </div>
-    </div>
-   </div>
+  			<div id="projects-section" class="clearfix">
+    			<div class="main-content">
+     				<h2 class="section-title">PROJECTS</h2>
+     				<div class="card card-flex-6 clickable-card">
+      					<figure class="card-header blue-tinted-header rounded-header">
+       						<img src="imgs/ios-development.jpg">
+      					</figure>
+      					<h3 class="card-title">iOS Development</h3>
+      					<p class="card-content">iOS Deveplopment has grown exponentially during the year of 2016. It appears more and more people are harnessing the power of mobile development, especially now that Apple's new programming language is gaining popularity.</p>
+     				</div>
+     				<div class="card card-flex-3 clickable-card">
+      					<figure class="card-header green-tinted-header rounded-header">
+       						<img src="imgs/ios-developer.jpg">
+      					</figure>
+      					<h3 class="card-title">iOS Development</h3>
+      					<p class="card-content">iOS Deveplopment has grown exponentially during the year of 2016. It appears more and more people are harnessing the power of mobile development, especially now that Apple's new programming language is gaining popularity.</p>
+     				</div>
+     				<div class="card card-flex-3 clickable-card">
+      					<figure class="card-header green-tinted-header rounded-header">
+       						<img src="imgs/ios-developer.jpg">
+      					</figure>
+      					<h3 class="card-title">iOS Development</h3>
+      					<p class="card-content">iOS Deveplopment has grown exponentially during the year of 2016. It$
+     				</div>
+    			</div>
+   			</div>
 
-   <div id="portfolio-section" class="clearfix">
+  			<div id="portfolio-section" class="clearfix">
     <div class="main-content">
      <h2 class="section-title">PORTFOLIO</h2>
      <div class="card card-flex-6 clickable-card">
@@ -98,27 +98,28 @@
     </div>
    </div>
 
-  </main>
+  		</main>
 
-  <footer id="footer">
-  	<div class="main-content">
-  		<ul class="footer-menu">
-  			<li><a href="#">Home</a></li>
-  			<li><a href="#">Projects</a></li>
-  			<li><a href="#">Portfolio</a></li>
-  			<li><a href="#">About Me</a></li>
-  			<li><a href="#">Contact</a></li>
-  		</ul>
-  		<ul class="social-media">
-  			<li><a href="#"><img class="social-media-img" src="imgs/linkedin.png" alt="LinkedIn"></a></li>
-  			<li><a href="#"><img class="social-media-img" src="imgs/github.png" alt="GitHub"></a></li>
-  			<li><a href="#"><img class="social-media-img" src="imgs/twitter.png" alt="Twitter"></a></li>
-  			<li><a href="#"><img class="social-media-img" src="imgs/instagram.png" alt="Instagram"></a></li>
-  			<li><a href="#"><img class="social-media-img" src="imgs/google+.png" alt="Google+"></a></li>
-  		</ul>
-  		<p id="copy">Copyright &copy; 2017 Bryan Morfe. All rights reserved.</p>
-  	</div>
-  </footer>
+  		<footer id="footer">
+  			<div class="main-content">
+  				<ul class="footer-menu">
+  					<li><a href="#">Home</a></li>
+  					<li><a href="#">Projects</a></li>
+  					<li><a href="#">Portfolio</a></li>
+  					<li><a href="#">About Me</a></li>
+  					<li><a href="#">Contact</a></li>
+  				</ul>
+  				<p id="copy">Copyright &copy; 2017 Bryan Morfe. All rights reserved.</p>
+  				<ul class="social-media">
+  					<li><a href="#"><img class="social-media-img" src="imgs/Linkedin.png" alt="LinkedIn"></a></li>
+  					<li><a href="#"><img class="social-media-img" src="imgs/GitHub.png" alt="GitHub"></a></li>
+  					<li><a href="#"><img class="social-media-img" src="imgs/Twitter.png" alt="Twitter"></a></li>
+  					<li><a href="#"><img class="social-media-img" src="imgs/Facebook.png" alt="Facebook"></a></li>
+  					<li><a href="#"><img class="social-media-img" src="imgs/Instagram.png" alt="Instagram"></a></li>
+  					<li><a href="#"><img class="social-media-img" src="imgs/Google+.png" alt="Google+"></a></li>
+  				</ul>
+  			</div>
+  		</footer>
   
  </body>
  
